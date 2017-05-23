7b36254423afd784a00fc179caebf0f09dfc8ae5
Alogy <1129507496@qq.com>
2017-05-23 18:57:54 +0800

    change merge_list

--- merge_list.cpp
+++ merge_list.cpp
@@ -54,23 +54,36 @@ public:
 //merge two sorted linked list, return a new list
 linkedlist merge(linkedlist a, linkedlist b){
   linkedlist result;
-  while(!a.empty() || !b.empty()){
+  while(!a.empty() || !b.empty()){ // 考虑极端情况， a 链表为空，把b链表中的一个个添加 结果链表中； b 链表为空，把a链表中的一个个添加 结果链表中
     if(a.empty()){
       result.push_back(b.front());
       b.pop_front();
     }else if(b.empty()){
       result.push_back(a.front());
       a.pop_front();
-    }else if(a.front() > b.front()){
+    }else if(a.front() > b.front()){ // a，b 链表都不为空， 看a和b 的head 谁大 (比较值的大小)。 a > b , b添加到结果链表中
       result.push_back(b.front());
       b.pop_front();
     }else{
-      result.push_back(a.front());
+      result.push_back(a.front()); // a < b ， a添加到结果链表中
       a.pop_front();
     }
   }
   return result;
 }
+
+/**
+逻辑上的循环，并不知道具体会循环多少次，**去看关键逻辑**。
+
+a的长度是m，b的长度是n。
+总共循环了`m+n`次。
+```
+(m+n) * O(1)
+
+时间复杂度为：
+O(m+n)
+```
+*/
 int main(){
 
   linkedlist a,b;
